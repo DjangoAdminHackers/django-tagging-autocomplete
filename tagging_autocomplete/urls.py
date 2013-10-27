86eea0647edf525cec92fb135f3197104dabdd55
Andy Baker <andy@andybak.net>
2013-10-27 16:28:23 +0000

    Fix for Django 1.5 compatibility

--- tagging_autocomplete/urls.py
+++ tagging_autocomplete/urls.py
@@ -1,4 +1,7 @@
-from django.conf.urls.defaults import *
+try:
+    from django.conf.urls.defaults import *
+except:
+    from django.conf.urls import *
 
 urlpatterns = patterns('tagging_autocomplete.views',
     url(r'^list$', 'list_tags', name='tagging_autocomplete-list'),
